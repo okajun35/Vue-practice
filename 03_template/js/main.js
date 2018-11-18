d9d7594d491c50af8959d590d4ec40f0e5d7c72f
Jun okazaki <hddwm390@yahoo.co.jp>
2018-11-18 11:40:57 +0900

    追加

--- 03_template/js/main.js
+++ 03_template/js/main.js
@@ -2,7 +2,8 @@ var app = new Vue({
     el: '#app',
     data: {
         message: 'hello Vue.js',
-        message2: 'Hello <span style="color:red;">Vue.js<span>'
+        message2: 'Hello <span style="color:red;">Vue.js<span>',
+        message3: 'hello Vue.js3',
     },
 
     methods:{
